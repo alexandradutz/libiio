dbe72b0d59d5b75b01a072a8c1f674cc595d6ab5
Travis CI <travis@travis-ci.org>
2018-01-30 07:42:47 +0000

    Deploy code docs to GitHub Pages

--- menudata.js
+++ menudata.js
@@ -22,4 +22,19 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main Page",url:"index.html"}]}
+{text:"Main Page",url:"index.html"},
+{text:"Modules",url:"modules.html"},
+{text:"Data Structures",url:"annotated.html",children:[
+{text:"Data Structures",url:"annotated.html"},
+{text:"Data Structure Index",url:"classes.html"},
+{text:"Data Fields",url:"functions.html",children:[
+{text:"All",url:"functions.html"},
+{text:"Variables",url:"functions_vars.html"}]}]},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"},
+{text:"Globals",url:"globals.html",children:[
+{text:"All",url:"globals.html",children:[
+{text:"i",url:"globals.html#index_i"}]},
+{text:"Functions",url:"globals_func.html",children:[
+{text:"i",url:"globals_func.html#index_i"}]},
+{text:"Enumerations",url:"globals_enum.html"}]}]}]}
